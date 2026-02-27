573313d1c166711e9b6b07785ad26fe1d38528ec
Usama Khan <usamakhan111se@gmail.com>
2026-02-27 16:20:15 +0500

    login day1

---
+++ NullNotes/src/components/Login.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from 'react'
+import {link, useNavigate} from 'react-router-dom'
+import {login as authLogin} from '../store/authSlice'
+import {Button, Input, logo} from './index'
+import {useDispatch} from 'react-redux'
+import authService from '../appwrite/auth'
+import {useForm} from 'react-hook-form'
+
+function Login() {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const {register, handleSubmit} = useForm()
+    const [error, setError] = useState("")
+    const login = async (data) => {
+        setError("")
+        try {
+            const session = await authService.login(data)
+            if(session) {
+                const userData = await authService.getCurrentUser()
+                if(userData) dispatch(authLogin(userData))
+                navigate("/")
+            }
+        } catch (error) {
+            setError(error.message)
+        }
+    }
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
